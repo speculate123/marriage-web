b2c5ffb187e89f2272390342fb46f22ed5f076a2
jeff-tseng <jeff.tseng@linecorp.com>
2026-05-05 11:35:45 +0800

    change supabase db

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 window.RSVP_CONFIG = {
-  SUPABASE_URL: "https://cvdiindbpfhouowxrjob.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_DoU2RV2Rt5b7GS98i6iyPw_1KG3aLW4",
+  SUPABASE_URL: "https://twvmcxfsyuymioqifybj.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_-6OxrgCD3osaXiOQlMlmEQ_31Daxdr2",
   TABLE_NAME: "rsvp_submissions",
   COOLDOWN_MS: 15000,
 };
